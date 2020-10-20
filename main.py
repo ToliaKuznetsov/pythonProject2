773380acbabdda7b1914741a852d262f5a89d004
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-10-20 19:27:14 +0700

    idecify

--- main.py
+++ main.py
@@ -1,13 +1,67 @@
+# Case-study #1
+# Developers:   Ivanov A. (20%),
+#               Petrova S. (60%),
+#               Sidorov M. (30%)
 import turtle
-turtle.goto(22.5,0)
-turtle.right(10)
-turtle.forward(46)
-turtle.goto(45,0)
-turtle.right(10)
-turtle.forward(45)
-turtle.goto(67.5,0)
-turtle.right(10)
-turtle.forward(46)
-turtle.goto(90,0)
-turtle.right(10)
-turtle.forward(45)
+
+
+def square(x, y, a):
+    '''
+    Function, drawing square.
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param a: side length of a square
+    :return: None
+    '''
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.down()
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(90)
+
+
+def triangle(x, y, a, ang, c):
+    '''
+    Function, drawing triangle.
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param a: side length of a triangle
+    :param ang: angle of a triangle
+    :param c: color of a triangle
+    :return: None
+    '''
+
+
+
+
+
+def parallelogram(x, y, a):
+    '''
+    Function, drawing square.
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param a: side length of a square
+    :return: None
+    '''
+
+
+def main():
+    '''
+    Main function.
+    :return: None
+    '''
+    square(-200, 200, 180)
+    square(20, 200, 180)
+    square(20, -20, 180)
+    square(-200, -20, 3456)
+    turtle.done()
+
+
+if __name__ == '__main__':
+    main()
