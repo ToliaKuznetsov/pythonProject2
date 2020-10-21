4de799d7699472ea2c45282fc691d49f333ba02f
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-10-21 22:21:53 +0700

    idecify

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #               Petrova S. (60%),
 #               Sidorov M. (30%)
 import turtle
-
+from turtle import *
 
 def square(x, y, a):
     '''
@@ -26,17 +26,41 @@ def square(x, y, a):
     turtle.right(90)
 
 
-def triangle(x, y, a, ang, c):
+
+def triangle(x, y, a, b, ang, c):
     '''
     Function, drawing triangle.
     :param x: upper left corner coordinate x
     :param y: upper left corner coordinate y
-    :param a: side length of a triangle
+    :param a:long side length of a triangle
+    :param b:short side length of a triangle
     :param ang: angle of a triangle
     :param c: color of a triangle
     :return: None
     '''
+turtle.up()
+turtle.setposition(x, y, ang)
+turtle.down()
+turtle.forward(b)
+turtle.right(90)
+turtle.forward(b)
+turtle.right(135)
+turtle.forward(a)
+turtle.right(135)
 
+
+def main():
+    '''
+    Main function.
+    :return: None
+    '''
+triangle(100, 100, 90)
+turtle.done()
+turtle.mainloop()
+
+if __name__ == '__main__':
+    main()
+turtle.mainloop()
 
 
 
@@ -56,12 +80,8 @@ def main():
     Main function.
     :return: None
     '''
-    square(-200, 200, 180)
-    square(20, 200, 180)
-    square(20, -20, 180)
-    square(-200, -20, 3456)
-    turtle.done()
-
+triangle(100, 100, 90)
+turtle.mainloop()
 
 if __name__ == '__main__':
     main()
