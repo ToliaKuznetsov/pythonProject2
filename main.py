b4a8f4b3a2559cddedeaa259e12a2379da1235a8
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-10-21 22:53:59 +0700

    idecigg

--- main.py
+++ main.py
@@ -3,19 +3,24 @@
 #               Petrova S. (60%),
 #               Sidorov M. (30%)
 import turtle
-
-
-def square(x, y, a):
+def square(x, y, a, angle, color):
     '''
     Function, drawing square.
     :param x: upper left corner coordinate x
     :param y: upper left corner coordinate y
     :param a: side length of a square
+    :param angle: rotation angle of a square
+    :param angle: rotation angle of a square (counterclockwise)
+    :param color: color of a square
     :return: None
     '''
     turtle.up()
     turtle.setposition(x, y)
+    turtle.setheading(angle)
+    turtle.left(angle)
+    turtle.color(color)
     turtle.down()
+    turtle.begin_fill()
     turtle.forward(a)
     turtle.right(90)
     turtle.forward(a)
@@ -24,6 +29,8 @@ def square(x, y, a):
     turtle.right(90)
     turtle.forward(a)
     turtle.right(90)
+    turtle.end_fill()
+    turtle.right(angle)
 
 
 def triangle(x, y, a, ang, c):
@@ -36,52 +43,39 @@ def triangle(x, y, a, ang, c):
     :param c: color of a triangle
     :return: None
     '''
-def triangle(x, y, a, b, ang, c):
+def parallelogram(x, y, a, b, c, r):
     '''
-    Function, drawing triangle.
-    :param x: upper left corner coordinate x
-    :param y: upper left corner coordinate y
-    :param a:long side length of a triangle
-    :param b:short side length of a triangle
-    :param ang: angle of a triangle
-    :param c: color of a triangle
-    :return: None
+    Function, drawing parallelogram.
+    :param x: upper  obtuse angle coordinate x
+    :param y: upper obtuse angle coordinate y
+    :param a: long side of the parallelogram
+    :param b: short side of the parallelogram
+    :param c: color
+    :param r: degree of rotation relative OX
     '''
-turtle.up()
-turtle.setposition(x, y, ang)
-turtle.down()
-turtle.forward(b)
-turtle.right(90)
-turtle.forward(b)
-turtle.right(135)
-turtle.forward(a)
-turtle.right(135)
-
-
-
-
-
-def parallelogram(x, y, a):
-    '''
-    Function, drawing square.
-    :param x: upper left corner coordinate x
-    :param y: upper left corner coordinate y
-    :param a: side length of a square
-    :return: None
-    '''
-
-
+    turtle.penup()
+    turtle.color(c)
+    turtle.setposition(x, y)
+    turtle.left(r)
+    turtle.pendown()
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(45)
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(45)
+    turtle.end_fill()
+    turtle.right(r)
 def main():
     '''
     Main function.
     :return: None
     '''
-    square(-200, 200, 180)
-    square(20, 200, 180)
-    square(20, -20, 180)
-    square(-200, -20, 3456)
-    turtle.done()
 
 
 if __name__ == '__main__':
+    main()
     main()
