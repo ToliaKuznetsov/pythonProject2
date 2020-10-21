70065f5a7cff5128a3fc045beb97bb07e1c0f7f2
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-10-21 22:43:54 +0700

    idecigg

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
-# Case-study #1
-# Developers:   Ivanov A. (20%),
-#               Petrova S. (60%),
-#               Sidorov M. (30%)
 import turtle
-
-
-def square(x, y, a):
+def square(x, y, a, angle, color):
     '''
     Function, drawing square.
     :param x: upper left corner coordinate x
     :param y: upper left corner coordinate y
     :param a: side length of a square
+    :param angle: rotation angle of a square (counterclockwise)
+    :param color: color of a square
     :return: None
     '''
+
     turtle.up()
     turtle.setposition(x, y)
-    turtle.down()
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
-    turtle.forward(a)
-    turtle.right(90)
+    turtle.left(angle)
+@@ -34,30 +34,27 @@ def square(x, y, a, angle, color):
 
 
 def triangle(x, y, a, ang, c):
-    '''
+    """
     Function, drawing triangle.
     :param x: upper left corner coordinate x
     :param y: upper left corner coordinate y
@@ -35,7 +25,37 @@ def triangle(x, y, a, ang, c):
     :param ang: angle of a triangle
     :param c: color of a triangle
     :return: None
+    """
+
+
+def parallelogram(x, y, a, b, c, r):
+    """
+    Function, drawing parallelogram.
+    :param x: upper  obtuse angle coordinate x
+    :param y: upper obtuse angle coordinate y
+    :param a: long side of the parallelogram
+    :param b: short side of the parallelogram
+    :param c: color
+    :param r: degree of rotation relative OX
+    """
+    turtle.penup()
+    turtle.color(c)
+    turtle.setposition(x, y)
+@@ -75,12 +72,12 @@ def parallelogram(x, y, a, b, c, r):
+    turtle.end_fill()
+    turtle.right(r)
+
+
+def main():
     '''
+    """
+    Main function.
+    :return: None
+    '''
+
+
+if __name__ == '__main__':
+    main()
 def triangle(x, y, a, b, ang, c):
     '''
     Function, drawing triangle.
