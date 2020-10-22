3b0db28d6539008039e8d52547033ff08fe9626f
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-10-23 01:00:48 +0700

    idecig

--- main.py
+++ main.py
@@ -34,6 +34,35 @@ def square(x, y, a, angle, color):
     turtle.right(angle)
 
 
+import turtle
+
+    '''
+    :return: Function, drawing triangle.
+    :param x: upper left corner coordinate x
+    :param y: upper left corner coordinate y
+    :param a: short-side length of a triangle
+    :param b: long-side lenght of a triangle
+    :param ang: rotation angle of a triangle
+    :param c: color of a triangle
+    :return: None
+   '''
+turtle.up()
+turtle.setposition(x, y)
+turtle.setheading(ang)
+turtle.down()
+turtle.color(c)
+turtle.begin_fill()
+turtle.forward(a)
+turtle.right(90)
+turtle.forward(a)
+turtle.right(135)
+turtle.forward(b)
+turtle.right(135)
+turtle.end_fill()
+def main():
+    triangle(1,2,10,20,0,"red")
+
+turtle.mainloop()
 
 
 
