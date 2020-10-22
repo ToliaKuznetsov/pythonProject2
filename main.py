762fe75c0dbe8e0dd26a90421755c7efd1b03c2f
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-10-23 01:48:43 +0700

    Merge remote-tracking branch 'origin/Tolia' into Tolia
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -36,33 +36,35 @@ def square(x, y, a, angle, color):
 
 import turtle
 def triangle(x, y, a, b, ang, c):
-    '''
-    :Function, drawing triangle.
+    """
+    Function, drawing triangle.
     :param x: upper left corner coordinate x
     :param y: upper left corner coordinate y
     :param a: short-side length of a triangle
-    :param b: long-side lenght of a triangle
+    :param b: long-side length of a triangle
+    :param b: long-side length of a triangle
     :param ang: rotation angle of a triangle
     :param c: color of a triangle
     :return: None
-    '''
-turtle.up()
-turtle.setposition(x, y)
-turtle.setheading(ang)
-turtle.down()
-turtle.color(c)
-turtle.begin_fill()
-turtle.forward(a)
-turtle.right(90)
-turtle.forward(a)
-turtle.right(135)
-turtle.forward(b)
-turtle.right(135)
-turtle.end_fill()
+    """
+    turtle.up()
+    turtle.setposition(x, y)
+    turtle.setheading(ang)
+    turtle.down()
+    turtle.color(c)
+    turtle.begin_fill()
+    turtle.forward(a)
+    turtle.right(90)
+    turtle.forward(a)
+    turtle.right(135)
+    turtle.forward(b)
+    turtle.right(135)
+    turtle.end_fill()
 def main():
-    triangle(1,2,10,20,0,"red")
-
+    triangle(1, 2, 10, 20, 0, "red")
 turtle.mainloop()
+if __name__ == '__main__':
+    main()
 
 
 
