794e40a84525edcc9a7911de66730fab3595b3e2
ToliaKuznetsov <73088456+ToliaKuznetsov@users.noreply.github.com>
2020-10-26 09:58:14 +0700

    Merge remote-tracking branch 'origin/Tolia' into Tolia
    # Conflicts:
    #	main.py

---
+++ main1.py
@@ -0,0 +1,13 @@
+a = float(input())
+b = a * 3,78541
+c = a * 0.0238095
+d = a * 19.6
+e = a * 114000
+f = a / 1.5 * 114000
+s = a * 3
+print (b)
+print (c)
+print (d)
+print (e)
+print (f)
+print (s)
